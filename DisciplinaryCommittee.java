6e87162b5fb0b50dc21c6e3279c9a8ab1e1d727d
AliReza <alireza_mzv@yahoo.com>
2020-03-02 01:44:55 +0330

    DisciplinaryCommittee

---
+++ DisciplinaryCommittee.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class DisciplinaryCommittee{
+
+    public static void checkIsAllowedPlayer(String playerName, String playerTeamName, String[] allowedPlayers, SortedSet<String>delinquentedTeams){
+        for(int i = 0; i < allowedPlayers.length; i++)
+            if(playerName.equals(allowedPlayers[i]))
+                return;
+
+        delinquentedTeams.add(playerTeamName+"\n");
+
+        return;
+    }
+
+    public static void checkHavingTeam(String playerName, String playerTeamName, String[][] teamsPlayers, int[] teamsPlayersCount, String[] teamsNames, 
+                                       SortedSet<String>delinquentedTeams){
+        for(int i = 0; i < teamsPlayers.length; i++)
+            for(int j = 0; j < teamsPlayersCount[i]; j++)
+                if(playerName.equals(teamsPlayers[i][j]) && !playerTeamName.equals(teamsNames[i])){
+                    delinquentedTeams.add(playerTeamName);
+                    delinquentedTeams.add(teamsNames[i]);
+                }
+
+        return;
+    }
+
+    public static void main(String args[]){
+        int n, m;
+        
+        Scanner scan = new Scanner(System.in);
+        n = scan.nextInt();
+        m = scan.nextInt();
+        scan.nextLine();
+
+        String[] allowedPlayers = new String[n];
+        String[] teamsNames = new String[m];
+        int[] teamsPlayersCount = new int[m];
+        String[][] teamsPlayers = new String[m][100];
+        SortedSet <String> delinquentTeams = new TreeSet<>();
+
+        for(int i = 0; i < n; i++)
+            allowedPlayers[i] = scan.nextLine();
+        
+      
+        for(int i = 0; i < m; i++){
+            teamsNames[i] = scan.nextLine();
+            teamsPlayersCount[i] = scan.nextInt();
+            scan.nextLine();
+            
+            for(int j = 0; j < teamsPlayersCount[i]; j++){
+                teamsPlayers[i][j] = scan.nextLine();
+
+                checkIsAllowedPlayer(teamsPlayers[i][j], teamsNames[i], allowedPlayers, delinquentTeams); 
+                checkHavingTeam(teamsPlayers[i][j], teamsNames[i], teamsPlayers, teamsPlayersCount, teamsNames, delinquentTeams);
+            }
+        }
+        scan.close();
+
+        while(delinquentTeams.size() > 0){
+            System.out.println(delinquentTeams.first());
+            delinquentTeams.remove(delinquentTeams.first());   
+        }
+    }
+
+}
